92aad435a979ac2cb588f5635229d91da4196ab5
Magio <aalbertobus@gmail.com>
2013-08-27 20:18:10 -0600

    first deploy

---
+++ public/js/main.js
@@ -0,0 +1,19 @@
+
+$(function(){
+	$('body').plusAnchor({
+        easing: 'linear',
+        speed:  1300
+    });
+
+    $('header').animate({
+    	opacity: 1,
+    	width: "101%"
+  	}, 1700, function() {
+    	$('header').animate({
+    		width:"100%"
+    	},100,function(){
+    		$('section').fadeIn(1200);
+    	});
+  	});
+
+});	
